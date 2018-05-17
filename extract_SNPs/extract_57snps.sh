f6b8872c805f42ce051ccffe5cef8a7e5f5d5cd1
Qian Yang <37040311+qy17115@users.noreply.github.com>
2018-05-17 16:48:33 +0100

    Update extract_57snps.sh

--- extract_SNPs/extract_57snps.sh
+++ extract_SNPs/extract_57snps.sh
@@ -1,9 +1,17 @@
 module add languages/python-2.7.6
+module add apps/matlab-r2015a
 
+###
+
+
+###
 cat insomnia57_new.gen | python gen_to_expected.py > insomnia57-dosage.txt
 
 cat insomnia57-dosage.txt | awk '{print NF}'
 
 cat insomnia57-dosage.txt | cut -d' ' -f7- > insomnia57-dosage2.txt
 
+### participants ID
 awk '(NR>2){print $1}' data_app2393.sample > userIds_app2393.txt
+
+
