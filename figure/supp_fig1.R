e1d910664f0d738c7e2ed15cb94eaffa867e3262
Qian Yang <37040311+qy17115@users.noreply.github.com>
2018-08-03 14:40:44 +0100

    Update supp_fig1.R

--- figure/supp_fig1.R
+++ figure/supp_fig1.R
@@ -5,11 +5,18 @@ colnames(sex)<-c("eid","eid2","sex","array")
 summary(sex)
 female<-sex[which(sex$sex=="F"),]
 
+#standard exclusion
+standard_ex<-read.table("O:/ukbb-sleep/genetic/data.combined_recommended_app2393.txt",,header = TRUE)
+colnames(standard_ex)<-c("eid","eid2")
+female_standard<-merge(female,standard_ex,by="eid",all=TRUE)
+female_exclude_standard<-female_standard[ which(is.na(female_standard$eid2.y)),]
+female_exclude_standard<-female_exclude_standard[c(1:4)]
+
 #minimal related
 minimal_r<-read.table("O:/ukbb-sleep/genetic/data.minimal_relateds_app2393.txt",header = TRUE)
 colnames(minimal_r)<-c("eid","eid2")
-female_minimal<-merge(female,minimal_r,by="eid",all=TRUE)
-female_exclude_minimal<-female_minimal[ which(is.na(female_minimal$eid2.y)),]
+female_minimal<-merge(female_exclude_standard,minimal_r,by="eid",all=TRUE)
+female_exclude_minimal<-female_minimal[ which(is.na(female_minimal$eid2)),]
 female_exclude_minimal<-female_exclude_minimal[c(1:4)]
 
 #highly related
