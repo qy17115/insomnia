4f4f8d5a191c461f26004504a509805442692003
Qian Yang <37040311+qy17115@users.noreply.github.com>
2018-08-03 13:55:16 +0100

    Create supp_fig1.R

---
+++ figure/supp_fig1.R
@@ -0,0 +1,25 @@
+#supplementary figure 1 participants flow diagram
+sex<-read.table("O:/ukbb-sleep/phenotype/data.covariates_app2393.txt",header = TRUE)
+head(sex)
+colnames(sex)<-c("eid","eid2","sex","array")
+summary(sex)
+female<-sex[which(sex$sex=="F"),]
+
+#minimal related
+minimal_r<-read.table("O:/ukbb-sleep/genetic/data.minimal_relateds_app2393.txt",header = TRUE)
+colnames(minimal_r)<-c("eid","eid2")
+female_minimal<-merge(female,minimal_r,by="eid",all=TRUE)
+female_exclude_minimal<-female_minimal[ which(is.na(female_minimal$eid2.y)),]
+female_exclude_minimal<-female_exclude_minimal[c(1:4)]
+
+#highly related
+high_r<-read.table("O:/ukbb-sleep/genetic/data.highly_relateds_app2393.txt",header = TRUE)
+colnames(high_r)<-c("eid","eid2")
+female_high<-merge(female_exclude_minimal,high_r,by="eid",all=TRUE)
+female_exclude_high<-female_high[ which(is.na(female_high$eid2)),]
+
+#phenotypic data
+phenotype<-read.csv("O:/ukbb-sleep/phenotype/insomnia_outcome4.csv")
+phenotype<-phenotype[c(1:3)]
+colnames(phenotype)<-c("no","eid","sex_q")
+female_final<-merge(phenotype,female_exclude_high,by="eid")
