1aa66576189fa2b50f2d611ae0e0fddf242ca7c0
Qian Yang <37040311+qy17115@users.noreply.github.com>
2018-11-07 08:26:33 +0000

    Update step3_check_pleiotropy_phenoscanner.R

--- genotype/step3_check_pleiotropy_phenoscanner.R
+++ genotype/step3_check_pleiotropy_phenoscanner.R
@@ -10,7 +10,7 @@ library(devtools)
 install_github("phenoscanner/phenoscanner")
 library(phenoscanner)
 
-snp<-read.csv("C:/mini project 2/results/insomnia71 in UKB.csv")
+snp<-read.csv("C:/mini project 2/results/insomnia80 in UKB.csv")
 res <- phenoscanner(snpquery=snp$rsid,pvalue=5e-08)
 head(res$results)
 res$snps
